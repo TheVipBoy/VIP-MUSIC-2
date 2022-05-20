9725fe5bde8d05f19642f50e107343f09d082cfe
adityabots <adityabots@yandex.com>
2022-05-20 16:42:06 +0530

    𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜𝐛𝐨𝐭

---
+++ README.md
@@ -0,0 +1,27 @@
+### Best Smart Voice Chat Music Robot For Telegram Groups ...
+
+
+<p align="center"><a href="https://t.me/adityahalder"><img src="https://te.legra.ph/file/c6e1041c6c9a12913f57a.png"></a></p>
+
+
+### Deploy On OkTeto
+
+[![Deploy+On+Okteto](https://img.shields.io/badge/Deploy%20To%20Okteto-informational?style=for-the-badge&logo=Okteto)](https://cloud.okteto.com/deploy?repository=https://github.com/adityabots/tgmusicbot)
+
+
+### Deploy To Heroku
+
+[![Deploy+On+Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/adityabots/tgmusicbot)
+
+
+
+### Get String Session
+
+[![GenerateString](https://img.shields.io/badge/repl.it-generateString-yellowgreen)](https://replit.com/@AdityaHalder/StringSession)
+
+
+
+#### Copy Pasters You Can Copy This But Must Give Credits ...
+
+### Owner Of This Repository
+[![Aditya Halder](https://te.legra.ph/file/8f9d2a593854d0c736201.png)](https://t.me/AdityaHalder)
---
+++ modules/config.py
@@ -0,0 +1,33 @@
+# 𝐌𝐨𝐝𝐮𝐥𝐞𝐬 𝐚𝐧𝐝 𝐄𝐧𝐯𝐢𝐫𝐨𝐧𝐦𝐞𝐧𝐭𝐬
+import os
+import aiohttp
+from os import getenv
+from dotenv import load_dotenv
+
+# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫)
+load_dotenv()
+que = {}
+admins = {}
+aiohttpsession = aiohttp.ClientSession()
+
+# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫
+API_HASH = getenv("API_HASH", "XXXXX")
+API_ID = int(getenv("API_ID", "XXXXX"))
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "XXXXX")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://te.legra.ph/file/c6e1041c6c9a12913f57a.png")
+BOT_NAME = getenv("BOT_NAME", "XXXXX")
+BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
+BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
+OWNER_NAME = getenv("OWNER_NAME", "𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "AdityaHalder")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/adityabots/tgmusicbot")
+STRING_SESSION = getenv("STRING_SESSION", "session")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5356564375").split()))
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "AdityaDiscus")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AdityaServer")
+
+# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫) 
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "kaalxd")
