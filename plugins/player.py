d180ae3ecfa18ecf584aa3c4c5e194322269783c
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-08-20 23:15:05 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -117,7 +117,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
 
 @Client.on_message(
-    commandpro(["/play", ".play", "!play", "play", "@", "#", "$"])
+    commandpro(["/mstart", ".play", "!play", "play", "@", "#", "$"])
     & filters.group
     & ~filters.edited
     & ~filters.forwarded
