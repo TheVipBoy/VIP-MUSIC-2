7804f062ef83bb31c1dbf8613251901122de0e29
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-08-21 09:02:46 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -31,7 +31,7 @@ async def _human_time_duration(seconds):
     
    
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(command("mstart") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{BOT_IMAGE}",
